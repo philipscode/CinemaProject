a65720055382b52ae257e56df66c16c408115adf
Филипп Поздняков <philipsletterbox@gmail.com>
2020-03-20 14:21:55 +0300

    swagger

--- Rambler.WebApi.Cinema/Startup.cs
+++ Rambler.WebApi.Cinema/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Rambler.WebApi.Cinema.Models;
 using Rambler.WebApi.Cinema.Profiles;
@@ -37,11 +38,23 @@ namespace Rambler.WebApi.Cinema
             services.AddAutoMapper(c => c.AddProfile<AutoMapperProfile>(), typeof(Startup));
 
             services.AddTransient<SessionService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Cinema API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cinema API V1");
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
